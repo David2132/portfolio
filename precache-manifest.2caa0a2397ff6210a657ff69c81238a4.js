81ede5548998f9659628252d4a47292abc2a2662
David2132 <48841577+David2132@users.noreply.github.com>
2022-01-11 23:41:08 -0600

    Updates

---
+++ precache-manifest.2caa0a2397ff6210a657ff69c81238a4.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05c74f64bed28137151676f8b1bfee89",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "5624ac5fc9e3d5bf6cb5",
+    "url": "/Portfolio/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b34b28d4ba08a9d0b9f3",
+    "url": "/Portfolio/static/css/main.a6f7549c.chunk.css"
+  },
+  {
+    "revision": "5624ac5fc9e3d5bf6cb5",
+    "url": "/Portfolio/static/js/2.b3beff9b.chunk.js"
+  },
+  {
+    "revision": "b34b28d4ba08a9d0b9f3",
+    "url": "/Portfolio/static/js/main.baa74307.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "f082aa91fa0b8c4658824e2e2a0f0c0e",
+    "url": "/Portfolio/static/media/1.f082aa91.PNG"
+  },
+  {
+    "revision": "0e96c00bf48a381eb64ef40e47e20880",
+    "url": "/Portfolio/static/media/Capture.0e96c00b.PNG"
+  },
+  {
+    "revision": "ef16bfcfe7cc686984051283182f0c56",
+    "url": "/Portfolio/static/media/Capture.ef16bfcf.PNG"
+  },
+  {
+    "revision": "ec84d12eae0cf8623667a50b4b21d088",
+    "url": "/Portfolio/static/media/Capture1.ec84d12e.PNG"
+  },
+  {
+    "revision": "f6a2a2d3751cd337c7c7b9837cbc471c",
+    "url": "/Portfolio/static/media/David.f6a2a2d3.jpg"
+  },
+  {
+    "revision": "e128db574afa9f78d99952ca953c73a9",
+    "url": "/Portfolio/static/media/addValidator.e128db57.PNG"
+  },
+  {
+    "revision": "e95e0566bb95d492966372823952c7ab",
+    "url": "/Portfolio/static/media/bird.e95e0566.jpg"
+  },
+  {
+    "revision": "9c82da6324059b7f9314a55465ce5aea",
+    "url": "/Portfolio/static/media/coffee.9c82da63.jpg"
+  },
+  {
+    "revision": "c5c54a0ac94a621df06ac5c5d4ded8c6",
+    "url": "/Portfolio/static/media/doggie.c5c54a0a.jpg"
+  },
+  {
+    "revision": "0b4e65247d4de88ccc614d54e2084dcf",
+    "url": "/Portfolio/static/media/doggies.0b4e6524.jpg"
+  },
+  {
+    "revision": "c308c3e5ee638821502cfcfc05012984",
+    "url": "/Portfolio/static/media/doggies2.c308c3e5.jpg"
+  },
+  {
+    "revision": "d5de18083d2cf518a0950e6d6ac3e1b0",
+    "url": "/Portfolio/static/media/email.d5de1808.png"
+  },
+  {
+    "revision": "d3d683e8a72de0b466816db16bfecddb",
+    "url": "/Portfolio/static/media/github-logo.d3d683e8.png"
+  },
+  {
+    "revision": "395df93f3a2d8a841031e95505e05fa4",
+    "url": "/Portfolio/static/media/header-background.395df93f.jpg"
+  },
+  {
+    "revision": "c66088431078114ee4a332577e43d43f",
+    "url": "/Portfolio/static/media/linkedin.c6608843.png"
+  }
+]);
